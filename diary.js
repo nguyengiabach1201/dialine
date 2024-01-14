89b3e777cb234b9a17e458733a7a9e50fc677336
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-01-14 12:13:40 +0700

    Add files via upload

---
+++ diary.js
@@ -0,0 +1,9 @@
+add(form1('12th January 2024!', 'Happy birthday to me! Today is really a fun and a really joyful day not only for me but also for my family too!', './imgs/jan12.jpg', 'jan12'));
+// add(br(6));
+// add(img('./imgs/bg/cloud.jpg'));
+// add(form2('12th January!', 'test', './imgs/test.jpg', 'jan15'));
+// add(img('./imgs/bg/balloon2.png','right'));
+
+// setTimeout(function () {
+//     connect('jan14', 'jan15');
+// }, 10);
